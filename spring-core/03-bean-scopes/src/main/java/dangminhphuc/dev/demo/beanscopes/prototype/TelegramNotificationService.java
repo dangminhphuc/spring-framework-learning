475a2dff63465f47f4d4a779e3d0004a22a83901
dangminhphuc <dangminhphuc.dev@gmail.com>
2025-05-04 23:06:09 +0700

    Refactor

--- spring-core/03-bean-scopes/src/main/java/dangminhphuc/dev/demo/beanscopes/prototype/TelegramNotificationService.java
+++ spring-core/03-bean-scopes/src/main/java/dangminhphuc/dev/demo/beanscopes/prototype/TelegramNotificationService.java
@@ -3,8 +3,17 @@ package dangminhphuc.dev.demo.beanscopes.prototype;
 import dangminhphuc.dev.demo.beanscopes.NotificationService;
 
 public class TelegramNotificationService implements NotificationService {
+
+    public void init() {
+        System.out.println("Initializing telegram notification service");
+    }
+
     @Override
     public void send(String message) {
-        System.out.println("Sending telegram notification from instance [" + this.hashCode() + "]: " + message);
+        System.out.println("Sending telegram notification: " + message);
+    }
+
+    public void destroy() {
+        System.out.println("Destroying telegram notification service");
     }
 }
--- spring-core/03-bean-scopes/src/main/java/dangminhphuc/dev/demo/beanscopes/singleton/EmailNotificationService.java
+++ spring-core/03-bean-scopes/src/main/java/dangminhphuc/dev/demo/beanscopes/singleton/EmailNotificationService.java
@@ -3,8 +3,17 @@ package dangminhphuc.dev.demo.beanscopes.singleton;
 import dangminhphuc.dev.demo.beanscopes.NotificationService;
 
 public class EmailNotificationService implements NotificationService {
+
+    public void init() {
+        System.out.println("Initializing email notification service");
+    }
+
     @Override
     public void send(String message) {
-        System.out.println("Sending email notification from instance [" + this.hashCode() + "]: " + message);
+        System.out.println("Sending email notification: " + message);
+    }
+
+    public void destroy() {
+        System.out.println("Destroying email notification service");
     }
 }
