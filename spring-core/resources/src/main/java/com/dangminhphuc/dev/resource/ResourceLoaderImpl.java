a0b5bc7fc2166dba2884bec5504b0d886d0f66e6
dangminhphuc <dangminhphuc.dev@gmail.com>
2025-06-04 01:25:04 +0700

    Resources: Init

---
+++ spring-core/resources/src/main/java/com/dangminhphuc/dev/resource/ResourceLoaderImpl.java
@@ -0,0 +1,12 @@
+package com.dangminhphuc.dev.resource;
+
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResourceLoaderImpl {
+
+    // TBU
+    private ResourceLoader resourceLoader;
+
+}
