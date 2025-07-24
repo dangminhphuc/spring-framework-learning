2e3336fd85d9efa00e373c872966e4e8e0a517f2
Đặng Minh Phúc <dangminhphuc.dev@gmail.com>
2025-07-24 23:46:19 +0700

    models

---
+++ model-example/src/main/java/com/dangminhphuc/dev/Bar.java
@@ -0,0 +1,14 @@
+package com.dangminhphuc.dev;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bar {
+    private int i;
+}
