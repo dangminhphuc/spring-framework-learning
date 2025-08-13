885dc420bbbf874d49cacb10af7b640a6b40a2a2
dangminhphuc <dangminhphuc.dev@gmail.com>
2025-08-13 22:38:29 +0700

    .

--- data-access/transaction-management/src/main/java/com/dangminhphuc/dev/transaction/BarServiceImpl.java
+++ data-access/transaction-management/src/main/java/com/dangminhphuc/dev/transaction/BarServiceImpl.java
@@ -3,23 +3,18 @@ package com.dangminhphuc.dev.transaction;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service("barService")
-public class BarServiceImpl implements Transactionaly {
+public class BarServiceImpl implements Transaction {
 
     private final JdbcTemplate jdbcTemplate;
+    private final List<String> instances = new ArrayList<>();
 
     public BarServiceImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-
-//    @Override
-//    @Transactional(propagation = Propagation.REQUIRES_NEW)
-//    public void insertBar(Bar bar) {
-////        System.out.println("--- Inside BarServiceImpl.insertBar (REQUIRES_NEW) ---");
-////        System.out.println("Inserting Bar: " + bar.getName());
-//        jdbcTemplate.update("INSERT INTO BAR (NAME) VALUES (?)", bar.getName());
 
     @Override
     public JdbcTemplate jdbcTemplate() {
@@ -31,57 +26,4 @@ public class BarServiceImpl implements Transactionaly {
         return "BAR";
     }
 
-    /// /        System.out.println("--- Exiting BarServiceImpl.insertBar ---");
-//    }
-//
-//    @Override
-//    public void insertWithRequiredPropagation() throws Exception {
-//        this.jdbcTemplate.update("INSERT INTO BAR (NAME) VALUES (?)", "INSERT WITH REQUIRED PROPAGATION");
-//    }
-//
-//    @Override
-//    public void executeFailure(String name) {
-//        this.jdbcTemplate.update("INSERT INTO BAR (NAME) VALUES (?)", name);
-//        throw new RuntimeException("Deliberate exception to trigger rollback in BarService");
-//    }
-//    @Override
-//    public void required_succeed() {
-//
-//    }
-
-//    @Override
-//    public void required_failure() {
-//
-//    }
-
-    @Override
-    public void requiredNew_succeed() {
-
-    }
-
-    @Override
-    public void requiredNew_failure() {
-
-    }
-
-    @Override
-    public void insertFooAndBarSuccessfully(Foo foo, Bar bar) {
-
-    }
-
-    @Override
-    public void insertFooAndBarWithCheckedException(Foo foo, Bar bar) throws IOException {
-
-    }
-
-    @Override
-    public void insertFooAndBarWithPropagation(Foo foo, Bar bar) {
-
-    }
-
-    @Override
-    public Integer count() {
-        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM BAR", Integer.class);
-        return (count == null) ? 0 : count;
-    }
 }
--- data-access/transaction-management/src/main/java/com/dangminhphuc/dev/transaction/GammaService.java
+++ data-access/transaction-management/src/main/java/com/dangminhphuc/dev/transaction/GammaService.java
@@ -1,31 +1,66 @@
 package com.dangminhphuc.dev.transaction;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class GammaService {
-    private final Transactionaly fooService;
-    private final Transactionaly barService;
+    private final Transaction fooService;
+    private final Transaction barService;
+
+    @Getter
+    private final List<String> steps = new ArrayList<>();
 
     @Autowired
-    public GammaService(@Qualifier("fooService") Transactionaly fooService, @Qualifier("barService") Transactionaly barService) {
+    public GammaService(@Qualifier("fooService") Transaction fooService,
+                        @Qualifier("barService") Transaction barService) {
         this.fooService = fooService;
         this.barService = barService;
     }
 
-
     @Transactional
-    public void executeBothSucceeds() {
-        this.fooService.required_succeed();
-        this.barService.required_succeed();
+    public void REQUIRED_execBothTxSucceed() {
+        this.fooService.PROPAGATION_REQUIRED(true);
+        this.barService.PROPAGATION_REQUIRED(true);
     }
 
     @Transactional
-    public void executeOnlyOneSucceeds() {
-        this.fooService.required_succeed();
-        this.barService.required_failure();
+    public void REQUIRED_execOnly1TxSucceed() {
+        this.fooService.PROPAGATION_REQUIRED(true);
+        this.fooService.PROPAGATION_REQUIRED(false);
+    }
+
+    @Transactional
+    public void REQUIRED_NEW_execBothTxSucceed() {
+        this.steps.add("START");
+        this.fooService.PROPAGATION_REQUIRED_NEW(true);
+        this.barService.PROPAGATION_REQUIRED_NEW(true);
+        this.steps.add("FINISH");
+    }
+
+    @Transactional
+    public void REQUIRED_NEW_execOnly1TxSucceed() {
+        this.steps.add("START");
+        this.fooService.PROPAGATION_REQUIRED_NEW(true);
+        this.barService.PROPAGATION_REQUIRED_NEW(false);
+        this.steps.add("FINISH");
+    }
+
+    @Transactional
+    public void NESTED_execBothTxSucceed() {
+        this.fooService.PROPAGATION_REQUIRED_NEW(true);
+        this.barService.PROPAGATION_REQUIRED_NEW(true);
+    }
+
+    @Transactional
+    public void NESTED_execOnly1TxSucceed() {
+        this.fooService.PROPAGATION_REQUIRED_NEW(true);
+        this.barService.PROPAGATION_REQUIRED_NEW(false);
     }
 }
