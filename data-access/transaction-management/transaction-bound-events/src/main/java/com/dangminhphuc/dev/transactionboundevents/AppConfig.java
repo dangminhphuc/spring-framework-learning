b782b35ba9bead5c479a8f72d152b0a4424c3552
dangminhphuc <dangminhphuc.dev@gmail.com>
2025-08-30 21:36:05 +0700

    transaction bound event

---
+++ data-access/transaction-management/transaction-bound-events/src/main/java/com/dangminhphuc/dev/transactionboundevents/AppConfig.java
@@ -0,0 +1,30 @@
+package com.dangminhphuc.dev.transactionboundevents;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.dangminhphuc.dev.transactionboundevents")
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
