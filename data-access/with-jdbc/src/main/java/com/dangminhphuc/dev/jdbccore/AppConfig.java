a5cf7babd772f398dddbc4ab72ffc8557811ff9e
dangminhphuc <dangminhphuc.dev@gmail.com>
2025-09-05 15:37:09 +0700

    Data Access: add Postgresql container

--- data-access/with-jdbc/src/main/java/com/dangminhphuc/dev/jdbccore/AppConfig.java
+++ data-access/with-jdbc/src/main/java/com/dangminhphuc/dev/jdbccore/AppConfig.java
@@ -1,11 +1,12 @@
 package com.dangminhphuc.dev.jdbccore;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -13,14 +14,33 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan("com.dangminhphuc.dev")
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+//    @Bean
+//    public DataSource dataSource() {
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .addScript("schema.sql")
+//                .build();
+//    }
 
     @Bean
     public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("schema.sql")
-                .build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 
     @Bean
